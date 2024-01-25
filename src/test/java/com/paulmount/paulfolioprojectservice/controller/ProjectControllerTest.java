6d579f68f9abb36289b4afd6566b4cf9e3c065c3
Paul Mount <paul.mount@gmail.com>
2024-01-25 14:14:08 -0500

    Updated CircleCI to JDK 17

--- src/test/java/com/paulmount/paulfolioprojectservice/controller/ProjectControllerTest.java
+++ src/test/java/com/paulmount/paulfolioprojectservice/controller/ProjectControllerTest.java
@@ -3,16 +3,31 @@ package com.paulmount.paulfolioprojectservice.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paulmount.paulfolioprojectservice.web.model.ProjectDto;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs()
+@ComponentScan(basePackages = "com.paulmount.paulfolioproject.web.mappers.ProjectMapper")
 @WebMvcTest
 class ProjectControllerTest {
 
@@ -26,8 +41,25 @@ class ProjectControllerTest {
 
     @Test
     void getProjectById() throws Exception {
-        mockMvc.perform(get("/api/v1/project/"+ UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        ConstrainedFields fields = new ConstrainedFields(ProjectDto.class);
+
+        mockMvc.perform(get("/api/v1/project/{projectId}",UUID.randomUUID().toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("project-get",pathParameters(
+                                parameterWithName("projectId").description("UUID of desired project")
+                        ),
+                        responseFields(
+                                fields.withPath("id").description("Id of Project"),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").description("Date this was added to the list"),
+                                fields.withPath("lastModifiedDate").description("Last updated here"),
+                                fields.withPath("projectName").description("Name of the Project"),
+                                fields.withPath("description").description("A brief description of what the project is/does"),
+                                fields.withPath("projectUrl").description("A link to see the thing in action, if available"),
+                                fields.withPath("projectSource").description("Link to the repository (only for public projects"),
+                                fields.sectionWithPath("tags").description("A list of the various tech pieces used in the project")
+                        )));
     }
 
     @Test
@@ -35,10 +67,35 @@ class ProjectControllerTest {
         ProjectDto projectDto = validProject;
         String projectJson = objectMapper.writeValueAsString(projectDto);
 
+        ConstrainedFields fields = new ConstrainedFields(ProjectDto.class);
+
         mockMvc.perform(post("/api/v1/project")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(projectJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("product-new",
+                        requestFields(
+                                fields.withPath("id").ignored(),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").description("Date this was added to the list"),
+                                fields.withPath("lastModifiedDate").description("Last updated here"),
+                                fields.withPath("projectName").description("Name of the Project"),
+                                fields.withPath("description").description("A brief description of what the project is/does"),
+                                fields.withPath("projectUrl").description("A link to see the thing in action, if available"),
+                                fields.withPath("projectSource").description("Link to the repository (only for public projects"),
+                                fields.sectionWithPath("tags").description("A list of the various tech pieces used in the project")
+                        ),
+                        responseFields(
+                                fields.withPath("id").ignored(),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").description("Date this was added to the list"),
+                                fields.withPath("lastModifiedDate").description("Last updated here"),
+                                fields.withPath("projectName").description("Name of the Project"),
+                                fields.withPath("description").description("A brief description of what the project is/does"),
+                                fields.withPath("projectUrl").description("A link to see the thing in action, if available"),
+                                fields.withPath("projectSource").description("Link to the repository (only for public projects"),
+                                fields.sectionWithPath("tags").description("A list of the various tech pieces used in the project")
+                        )));
     }
 
     @Test
@@ -46,15 +103,63 @@ class ProjectControllerTest {
         ProjectDto projectDto = validProject;
         String projectJson = objectMapper.writeValueAsString(projectDto);
 
-        mockMvc.perform(put("/api/v1/project/"+UUID.randomUUID() )
+        ConstrainedFields fields = new ConstrainedFields(ProjectDto.class);
+
+        mockMvc.perform(put("/api/v1/project/{projectId}",UUID.randomUUID() )
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(projectJson))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("project-update",pathParameters(
+                                parameterWithName("projectId").description("UUID of desired project")
+                        ),
+                        requestFields(
+                                fields.withPath("id").ignored(),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").description("Date this was added to the list"),
+                                fields.withPath("lastModifiedDate").description("Last updated here"),
+                                fields.withPath("projectName").description("Name of the Project"),
+                                fields.withPath("description").description("A brief description of what the project is/does"),
+                                fields.withPath("projectUrl").description("A link to see the thing in action, if available"),
+                                fields.withPath("projectSource").description("Link to the repository (only for public projects"),
+                                fields.sectionWithPath("tags").description("A list of the various tech pieces used in the project")
+                        ),responseFields(
+                                fields.withPath("id").description("Id of Project"),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").description("Date this was added to the list"),
+                                fields.withPath("lastModifiedDate").description("Last updated here"),
+                                fields.withPath("projectName").description("Name of the Project"),
+                                fields.withPath("description").description("A brief description of what the project is/does"),
+                                fields.withPath("projectUrl").description("A link to see the thing in action, if available"),
+                                fields.withPath("projectSource").description("Link to the repository (only for public projects"),
+                                fields.sectionWithPath("tags").description("A list of the various tech pieces used in the project")
+                        )));
     }
 
     @Test
     void deleteProject() throws Exception {
-        mockMvc.perform(delete("/api/v1/project/"+UUID.randomUUID()))
-                .andExpect(status().isNoContent());
+        mockMvc.perform(delete("/api/v1/project/{projectId}",UUID.randomUUID()))
+                .andExpect(status().isNoContent())
+                .andDo(document("project-delete",pathParameters(
+                        parameterWithName("projectId").description("UUID of desired project")
+                )));
+    }
+
+    private static class ConstrainedFields {
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path),". ")));
+        }
+        private FieldDescriptor sectionWithPath(String path) {
+            return subsectionWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path),". ")));
+        }
     }
 }
